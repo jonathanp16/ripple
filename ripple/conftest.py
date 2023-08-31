95dacad3be72f859bf4ba06df5049eec229aa082
jonathanp16 <perezjonathan301@gmail.com>
2023-08-31 02:01:36 -0400

    main push

---
+++ ripple/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from ripple.users.models import User
+from ripple.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
